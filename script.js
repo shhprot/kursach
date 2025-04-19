e29cb41b1f212bbbe55586304c3923dbea6a2bf3
Crystal-hawk <roninsasa207@gmail.com>
2025-04-19 18:52:32 +0200

    kursach

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 'use strict'
 
-
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+        
+        
+        window.addEventListener('scroll', function() {
+            if (window.pageYOffset > 300) {
+                scrollTopBtn.classList.add('visible');
+            } else {
+                scrollTopBtn.classList.remove('visible');
+            }
+        });
+        
+        
+        scrollTopBtn.addEventListener('click', function() {
+            document.getElementById('header').scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
